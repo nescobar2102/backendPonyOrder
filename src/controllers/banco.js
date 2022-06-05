c054c2c54d85a29117b38c9ca12a855688511362
haisbely <nunezhaisbely@gmail.com>
2022-06-05 03:26:04 -0500

    sincronizacion y validacion de banco

--- src/controllers/banco.js
+++ src/controllers/banco.js
@@ -6,14 +6,15 @@ class Banco
         let results = await db.query(`SELECT * FROM banco ORDER BY nit ASC`).catch(console.log); 
         return results.rows;
     }
-    async getBancoByNit(nit) {
-        let results = await db.query('SELECT * FROM banco WHERE nit = $1', [nit]).catch(console.log); 
+    async getBancoByNit(nit,descripcion)
+        {
+        let results = await db.query('SELECT * FROM banco WHERE nit = $1 and descripcion =$2', [nit,descripcion]).catch(console.log); 
         return results.rows;
     }
-    async createBanco ( nit, id_banco, descripcion, id_tercero, id_sucursal_tercero ) {
-        let results = await db.query('SELECT * FROM banco WHERE nit = $1 and id_banco = $2', [nit, id_banco]).catch(console.log);
+    async createBanco (nit, id_banco, descripcion, id_tercero, id_sucursal_tercero) {
+        let results = await db.query('SELECT * FROM banco WHERE nit = $1 and descripcion = $2', [nit, descripcion]).catch(console.log);
         if (results.rowCount == 0) { 
-            await db
+            return await db
             .query('INSERT INTO banco (nit, id_banco, descripcion, id_tercero, id_sucursal_tercero) VALUES ($1, $2, $3, $4,$5)', [
                 nit, 
                 id_banco,
@@ -21,10 +22,8 @@ class Banco
                 id_tercero, 
                 id_sucursal_tercero
             ])
-            .catch(console.log);
-          return;
+            .catch(console.log);         
          }
         }
-
 }
 module.exports = Banco;
--- src/routers/banco.js
+++ src/routers/banco.js
@@ -6,48 +6,63 @@ const Banco = require('../controllers/banco');
 router.get('/banco_all', async (req,res) => {
     const response = newResponseJson();
     response.msg = 'Listar todos los Banco';
+    let status = 200;
     let banco = await new Banco().getBanco();
     if (banco.length>0){
         response.data = banco;
     }
     else {
+        status = 404;
         response.success = false;
+        response.mg = 'No existen registros';
     }
-    res.status(200).json(response)
+    res.status(status).json(response)
 });
 
 // listar bancos por Nit
-router.get('/banco/:nit', async (req,res) => {
+router.get('/banco/:nit/:descripcion', async (req,res) => {
     const response = newResponseJson();
     response.msg = 'Listar los Banco por Nit';
-    let {nit} = req.params;    
-    let banco = await new Banco().getBancoByNit(nit);
+    let status = 200;
+    let {nit,descripcion} = req.params;    
+    let banco = await new Banco().getBancoByNit(nit,descripcion);
     if (banco.length>0){
         response.data = banco;
     }
     else {
+        status = 404;
         response.success = false;
+        response.mg = 'No existen registros';  
     }
-    res.status(200).json(response)
+    res.status(status).json(response)
 });
 //Create a todo.
 router.post('/synchronization_banco', async (req,res) => {
     const response = newResponseJson();
     response.msg = 'Sincronización de Banco';
+    let status = 201;
     const {bancos} = req.body
+    let bandera = false;
     for (var i=0;i<bancos.length;i++){ 
         const { nit, id_banco, descripcion, id_tercero, id_sucursal_tercero } =  bancos[i]
-        await new Banco().createBanco( nit, id_banco, descripcion, id_tercero, id_sucursal_tercero ); 
-     };
-     if (bancos.length>0){
-        response.data = bancos;
+        result1 = await new Banco().createBanco( nit, id_banco, descripcion, id_tercero, id_sucursal_tercero ); 
+     
+        console.log('primer insert', result1?.rowCount);
+    if (!result1?.rowCount || result1?.rowCount == 0) {
+        console.log('no se hizo el insert');
+        bandera = true;
+        break;        
+    }
+}
+     if (bancos.length>0 && !bandera){
+        response.data = await new Banco().getBanco();
     }
     else {
         response.success = false;
+        status = 400;
+        response.msg = 'Error en la sincronización de banco';
     }
-    res.status(200).json(response)     
-    let banco_all= await new Banco().getBanco();
-    res.status(200).json(banco_all)  
+    res.status(200).json(response)         
 });
 function newResponseJson() {
     return {
