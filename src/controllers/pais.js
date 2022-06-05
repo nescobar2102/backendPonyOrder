0865721cd68aea9ce8b8a9bcdeaa4aa4ad5e1a3a
haisbely <nunezhaisbely@gmail.com>
2022-06-05 12:37:27 -0500

    sincronizacion y validacion pais

--- src/controllers/pais.js
+++ src/controllers/pais.js
@@ -6,14 +6,14 @@ class Pais
             let results = await db.query(`SELECT * FROM pais  ORDER BY nit ASC`).catch(console.log); 
             return results.rows;
         }
-        async getPaisByNit(nit) {
-            let results = await db.query('SELECT * FROM pais WHERE  nit = $1', [nit]).catch(console.log); 
+        async getPaisByNit(nit,nombre) {
+            let results = await db.query('SELECT * FROM pais WHERE  nit = $1 and nombre = $2', [nit,nombre]).catch(console.log); 
             return results.rows;
         }
         async createPais(nit,id_pais,ie_pais,nacionalidad,nombre) { 
-            let results = await db.query('SELECT * FROM pais WHERE nit = $1', [nit]).catch(console.log);
+            let results = await db.query('SELECT * FROM pais WHERE nit = $1 and nombre = $2 ', [nit,nombre]).catch(console.log);
             if (results.rowCount == 0) {     
-                await db
+                return await db
                 .query('INSERT INTO pais (nit,id_pais,ie_pais,nacionalidad,nombre) VALUES ($1, $2, $3, $4, $5)', [
                     nit,
                     id_pais,
@@ -21,8 +21,7 @@ class Pais
                     nacionalidad,
                     nombre
                 ])
-                .catch(console.log);
-              return;
+                .catch(console.log);              
             }
     }    
     }
--- src/routers/pais.js
+++ src/routers/pais.js
@@ -1,25 +1,72 @@
 const express = require("express");
 const router = express.Router();
 const Pais = require('../controllers/pais');
-
+//// listar todos los paises
 router.get('/pais_all', async (req,res) => { 
+    const response = newResponseJson();
+    response.msg = 'Listar todos los pais';
+    let status = 200;
     let pais = await new Pais().getPais(); 
-    res.status(200).json(pais)
+    if (pais.length>0){
+        response.data = pais;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
-router.get('/pais/:nit', async (req,res) => {
-    let {nit} = req.params;    
-    let pais = await new Pais().getPaisByNit(nit);
-    res.status(200).json(pais)
+//// listar  los paises por nit y nombre
+router.get('/pais/:nit/:nombre', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Listar los pais por Nit y nombre';
+    let status = 200;
+    let {nit,nombre} = req.params;    
+    let pais = await new Pais().getPaisByNit(nit,nombre);
+    if (pais.length>0){
+        response.data = pais;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';  
+    }
+    res.status(status).json(response)
 });
+/// sincronizacion  pais
 router.post('/synchronization_pais', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Sincronización de Pais';
+    let status = 201;
     const {paises } = req.body
+    let bandera = false;
     for (var i=0;i<paises.length;i++){ 
         const {nit,id_pais,ie_pais,nacionalidad,nombre} =  paises[i]
-        await new Pais().createPais(nit,id_pais,ie_pais,nacionalidad,nombre); 
-     };
-     
-     let pais_all= await new Pais().getPais();
-     res.status(200).json(pais_all)
-  
+        result1 = await new Pais().createPais(nit,id_pais,ie_pais,nacionalidad,nombre); 
+        
+        console.log('primer insert', result1?.rowCount);
+        if (!result1?.rowCount || result1?.rowCount == 0) {
+        console.log('no se hizo el insert');
+        bandera = true;
+        break;        
+        }
+    }
+        if (paises.length>0 && !bandera){
+        response.data = await new Pais().getPais();
+    }
+    else {
+        response.success = false;
+        status = 400;
+        response.msg = 'Error en la sincronización de Pais';
+    }
+    res.status(200).json(response)         
 });
+function newResponseJson() {
+    return {
+        success: true,
+        msg: "",
+        data: [],
+    };
+}
 module.exports = router;
