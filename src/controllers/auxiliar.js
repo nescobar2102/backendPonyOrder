62267723e61b6bec4c92f0aa5b4cfc0964ef74a4
haisbely <nunezhaisbely@gmail.com>
2022-06-05 11:50:13 -0500

    validacion y sincronizacion de auxiliar

--- src/controllers/auxiliar.js
+++ src/controllers/auxiliar.js
@@ -6,14 +6,14 @@ class Auxiliares
         let results = await db.query(`SELECT * FROM auxiliar ORDER BY nit ASC`).catch(console.log); 
         return results.rows;
     }
-    async getAuxiliaresByNit(nit) {
-        let results = await db.query('SELECT * FROM auxiliar WHERE nit = $1', [nit]).catch(console.log); 
+    async getAuxiliaresByNit(nit,descripcion) {
+        let results = await db.query('SELECT * FROM auxiliar WHERE nit = $1 and descripcion=$2', [nit,descripcion]).catch(console.log); 
         return results.rows;
     }
     async createAuxiliares (nit, id_auxiliar, descripcion, flag_flujo_caja, id_tipo_cuenta ) {
-        let results = await db.query('SELECT * FROM auxiliar WHERE nit = $1 and id_auxiliar = $2', [nit, id_auxiliar]).catch(console.log);
+        let results = await db.query('SELECT * FROM auxiliar WHERE nit = $1 and descripcion = $2', [nit, descripcion]).catch(console.log);
         if (results.rowCount == 0) {  
-            await db
+            return await db
             .query('INSERT INTO auxiliar (nit, id_auxiliar, descripcion, flag_flujo_caja, id_tipo_cuenta) VALUES ($1, $2, $3, $4,$5)', [
                 nit, 
                 id_auxiliar, 
@@ -22,9 +22,7 @@ class Auxiliares
                 id_tipo_cuenta
             ])
             .catch(console.log);
-          return;
             }
         }
-
 }
 module.exports = Auxiliares;
--- src/routers/auxiliar.js
+++ src/routers/auxiliar.js
@@ -4,26 +4,72 @@ const Auxiliares = require('../controllers/auxiliar');
 
 // listar Auxiliar
 router.get('/auxiliar_all', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Listar todos los Auxiliar';
+    let status = 200;
     let auxiliar = await new Auxiliares().getAuxiliares();
-    res.status(200).json(auxiliar)
+    
+    if (auxiliar.length>0){
+        response.data = auxiliar;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
+
 // listar Auxiliar por Nit
-router.get('/auxiliar/:nit', async (req,res) => {
-    let {nit} = req.params;    
-    let auxiliar = await new Auxiliares().getAuxiliaresByNit(nit);
-    res.status(200).json(auxiliar)
+router.get('/auxiliar/:nit/:descripcion', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Listar los Auxiliar por Nit y descripcion';
+    let status = 200;
+    let {nit,descripcion} = req.params;    
+    let auxiliar = await new Auxiliares().getAuxiliaresByNit(nit,descripcion);
+    if (auxiliar.length>0){
+        response.data = auxiliar;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';  
+    }
+    res.status(status).json(response)
 });
 //Create a todo.
 router.post('/synchronization_auxiliar', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Sincronización de Auxiliar';
+    let status = 201;
     const {auxiliares} = req.body
+    let bandera = false;
     for (var i=0;i<auxiliares.length;i++){ 
         const {nit, id_auxiliar, descripcion, flag_flujo_caja, id_tipo_cuenta } =  auxiliares[i]
-        await new Auxiliares().createAuxiliares( nit, id_auxiliar, descripcion, flag_flujo_caja, id_tipo_cuenta ); 
-     };
-     
-     let auxiliar_all= await new Auxiliares().getAuxiliares();
-     res.status(200).json(auxiliar_all)
-  
+        result1  = await new Auxiliares().createAuxiliares( nit, id_auxiliar, descripcion, flag_flujo_caja, id_tipo_cuenta ); 
+        
+        console.log('primer insert', result1?.rowCount);
+        if (!result1?.rowCount || result1?.rowCount == 0) {
+        console.log('no se hizo el insert');
+        bandera = true;
+        break;        
+    }
+}
+        if (auxiliares.length>0 && !bandera){
+        response.data = await new Auxiliares().getAuxiliares();
+}
+    else {
+    response.success = false;
+    status = 400;
+    response.msg = 'Error en la sincronización de auxiliar';
+}
+    res.status(status).json(response)         
 });
-
+    function newResponseJson() {
+        return {
+            success: true,
+            msg: "",
+            data: [],
+        };
+    }
 module.exports = router;
