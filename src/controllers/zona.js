04cbc700f58f75b19bbcfee01a0a6fa5fe60f45e
haisbely <nunezhaisbely@gmail.com>
2022-06-05 23:38:17 -0500

    sincronizacion y validaciones de zona

--- src/controllers/zona.js
+++ src/controllers/zona.js
@@ -6,14 +6,14 @@ class Zona
         let results = await db.query(`SELECT * FROM zona  ORDER BY nit ASC`).catch(console.log); 
         return results.rows;
     }
-    async getZonaByDesc(descripcion , nit) {
-        let results = await db.query('SELECT * FROM zona WHERE descripcion = $1 and nit = $2', [descripcion,nit]).catch(console.log); 
+    async getZonaByDesc(nit,descripcion) {
+        let results = await db.query('SELECT * FROM zona WHERE nit = $1 and descripcion = $2', [nit,descripcion]).catch(console.log); 
         return results.rows;
     }
     async createZona(id_zona , descripcion, id_padre, nivel, es_padre, nit) { 
-        let results = await db.query('SELECT * FROM zona WHERE nit = $1 and descripcion = $2', [descripcion,nit]).catch(console.log);
+        let results = await db.query('SELECT * FROM zona WHERE nit = $1 and descripcion = $2', [nit,descripcion]).catch(console.log);
         if (results.rowCount == 0) {     
-            await db
+            return await db
             .query('INSERT INTO zona (id_zona , descripcion, id_padre, nivel, es_padre, nit) VALUES ($1, $2, $3, $4, $5, $6)', [
             id_zona ,
             descripcion, 
@@ -22,8 +22,7 @@ class Zona
             es_padre, 
             nit
             ])
-            .catch(console.log);
-          return;
+            .catch(console.log);         
         }
 }
 }
--- src/routers/zona.js
+++ src/routers/zona.js
@@ -3,23 +3,70 @@ const router = express.Router();
 const Zona = require('../controllers/zona');
 
 router.get('/zona_all', async (req,res) => { 
+    const response = newResponseJson();
+    response.msg = 'Listar todas las zona';
+    let status = 200;
     let zona = await new Zona().getZona(); 
-    res.status(200).json(zona)
+    if (zona.length>0){
+        response.data = zona;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
-router.get('/zona/:descripcion/:nit', async (req,res) => {
-    let {descripcion,nit} = req.params;    
-    let zona = await new Zona().getZonaByDesc(descripcion,nit);
-    res.status(200).json(zona)
+
+router.get('/zona/:nit/:descripcion', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Listar las zona por Nit y descripcion';
+    let status = 200;
+    let {nit,descripcion} = req.params;    
+    let zona = await new Zona().getZonaByDesc(nit,descripcion);
+    if (zona.length>0){
+        response.data = zona;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';  
+    }
+    res.status(status).json(response)
 });
+
 router.post('/synchronization_zona', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Sincronización de zona';
+    let status = 201;
     const {zonas } = req.body
+    let bandera = false;
     for (var i=0;i<zonas.length;i++){ 
         const {id_zona , descripcion, id_padre, nivel, es_padre, nit} =  zonas[i]
-        await new Zona().createZona(id_zona , descripcion, id_padre, nivel, es_padre, nit ); 
-     };
-     
-     let zona_all= await new Zona().getZona();
-     res.status(200).json(zona_all)
-  
+        result1 = await new Zona().createZona(id_zona , descripcion, id_padre, nivel, es_padre, nit ); 
+        
+        console.log('primer insert', result1?.rowCount);
+        if (!result1?.rowCount || result1?.rowCount == 0) {
+        console.log('no se hizo el insert');
+        bandera = true;
+        break;        
+        }
+    }
+        if (zonas.length>0 && !bandera){
+        response.data = await new Zona().getZona();
+    }
+    else {
+        response.success = false;
+        status = 400;
+        response.msg = 'Error en la sincronización de zona';
+    }
+    res.status(200).json(response)         
 });
+function newResponseJson() {
+    return {
+        success: true,
+        msg: "",
+        data: [],
+    };
+}
 module.exports = router;
