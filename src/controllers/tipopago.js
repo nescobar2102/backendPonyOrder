94e379a1403c76cea8405bce1670e68c9323328a
haisbely <nunezhaisbely@gmail.com>
2022-06-05 23:25:15 -0500

    sincronizacion y validaciones de tipo de pago

--- src/controllers/tipopago.js
+++ src/controllers/tipopago.js
@@ -6,35 +6,34 @@ class Tipopago
         let results = await db.query(`SELECT * FROM tipo_pago  ORDER BY nit ASC `).catch(console.log); 
         return results.rows;
     }
-    async getTipopagoByDesc(descripcion , nit) {
-        let results = await db.query('SELECT * FROM tipo_pago WHERE descripcion = $1 and nit = $2', [descripcion,nit]).catch(console.log); 
+    async getTipopagoByDesc(nit,descripcion) {
+        let results = await db.query('SELECT * FROM tipo_pago WHERE nit = $1 and descripcion = $2', [nit,descripcion]).catch(console.log); 
         return results.rows;
     }
+    async deleteTipopago() {
+        await db.query(`DELETE FROM tipo_pago`).catch(console.log); 
+        await db.query(`DELETE FROM tipo_pagodet`).catch(console.log);        
+  }
     async createTipopago(id_tipo_pago, descripcion, nit ) { 
-        let results = await db.query('SELECT * FROM tipo_pago WHERE nit = $1 and descripcion = $2', [descripcion,nit]).catch(console.log);
+        let results = await db.query('SELECT * FROM tipo_pago WHERE nit = $1 and id_tipo_pago = $2', [nit,id_tipo_pago]).catch(console.log);
         if (results.rowCount == 0) {     
-            await db
+            return  await db
             .query('INSERT INTO tipo_pago (id_tipo_pago, descripcion, nit) VALUES ($1, $2, $3 )', [
                 id_tipo_pago, 
                 descripcion,
                 nit
             ])
-            .catch(console.log);
-          return;
+            .catch(console.log);          
         }
 }
     async createTipopagodet(nit,id_tipo_pago,id_auxiliar,cuota,clase) {
-        let results = await db.query('SELECT * FROM tipo_pagodet WHERE nit = $1 and id_tipo_pago =$2', [nit,id_tipo_pago]).catch(console.log);
-        if (results.rowCount == 0) {
-        await db.query('INSERT INTO tipo_pagodet (nit,id_tipo_pago,id_auxiliar,cuota,clase) VALUES ($1, $2, $3, $4,$5)', [
+            return await db.query('INSERT INTO tipo_pagodet (nit,id_tipo_pago,id_auxiliar,cuota,clase) VALUES ($1, $2, $3, $4,$5)', [
             nit,
             id_tipo_pago,
             id_auxiliar,
             cuota,
             clase      
         ]).catch(console.log);
-        return;
     }
-}
 }
 module.exports = Tipopago;
--- src/routers/tipopago.js
+++ src/routers/tipopago.js
@@ -3,27 +3,91 @@ const router = express.Router();
 const Tipopago = require('../controllers/tipopago');
 
 router.get('/tipopago_all', async (req,res) => { 
+    const response = newResponseJson();
+    response.msg = 'Listar todos los Tipo de pago';
+    let status = 200;
     let tipopago = await new Tipopago().getTipopago(); 
-    res.status(200).json(tipopago)
+    if (tipopago.length > 0) {
+        response.data = tipopago;
+    }
+    else {
+        status= 404;
+        response.success = false;
+        response.msg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
-router.get('/tipopago/:descripcion/:nit', async (req,res) => {
-    let {descripcion,nit} = req.params;    
-    let tipopago = await new Tipopago().getTipopagoByDesc(descripcion,nit);
-    res.status(200).json(tipopago)
+
+router.get('/tipopago/:nit/:descripcion', async (req,res) => {
+    const response = newResponseJson();
+    let status = 200;
+    response.msg = 'Listar un tipo de pago por Descripcion y Nit';
+    let {nit,descripcion} = req.params;    
+    let tipopago = await new Tipopago().getTipopagoByDesc(nit,descripcion);
+    if (tipopago.length > 0) {
+        response.data = tipopago;
+    }
+    else {
+        status= 404;
+        response.success = false;
+        response.msg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
+
 router.post('/synchronization_tipopago', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Sincronización de tipo pago';
+    let status = 201;
     const {tipopagos} = req.body
-    for (var i=0;i<tipopagos.length;i++){        
+    let bandera = false;
+    let bandera_hijo = false;
+    await new Tipopago().deleteTipopago(); 
+    for (var i=0;i<tipopagos.length;i++){     
+        if (!bandera_hijo) {   
         const {id_tipo_pago, descripcion, nit,tipopagosdet} =  tipopagos[i];
-            await new Tipopago().createTipopago(id_tipo_pago, descripcion, nit); 
+           result1 = await new Tipopago().createTipopago(id_tipo_pago, descripcion, nit); 
            
-            for (var j=0;j<tipopagosdet.length;j++){ 	            
+           console.log('primer insert', result1?.rowCount);
+           if (!result1?.rowCount || result1?.rowCount == 0) { 
+            console.log('entra al falase'); 
+            bandera = true;     
+            break;
+        } else {
+            if (tipopagosdet?.length > 0 && result1?.rowCount > 0) { 
+                console.log('pasa por el dto insert', result1?.rowCount);
+            
+                for (var j=0;j<tipopagosdet.length;j++){ 	            
                 const {nit,id_tipo_pago,id_auxiliar,cuota,clase} =  tipopagosdet[j];
-                await new Tipopago().createTipopagodet(nit,id_tipo_pago,id_auxiliar,cuota,clase); 
-           };
-     };     
-    let tipopago_all= await new Tipopago().getTipopago();
-     res.status(200).json(tipopago_all)
-  
+                result2 = await new Tipopago().createTipopagodet(nit,id_tipo_pago,id_auxiliar,cuota,clase); 
+                
+                if (!result2?.rowCount || result2?.rowCount == 0) {                                         bandera_hijo = true;    
+                    break;
+                }
+            }
+        }
+    }
+ }
+}  
+if (!bandera && !bandera_hijo) { //no se levanto la bandera (false)
+    let tipopago_all = await new Tipopago().getTipopago();
+    response.data = tipopago_all;
+} 
+else 
+{ 
+    await new Tipopago().deleteTipopago();
+    response.success = false;
+    status = 400;
+    response.msg = 'Error en la  sincronización de tipo de pago';
+}
+res.status(status).json(response);
+
 });
+function newResponseJson() {
+return {
+    success: true,
+    msg: "",
+    data: [],
+};
+}
 module.exports = router;
