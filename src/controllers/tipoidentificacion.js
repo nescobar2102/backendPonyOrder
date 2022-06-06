8058b42b1f31fb7a9e407c3bf0322fc82feb55b0
haisbely <nunezhaisbely@gmail.com>
2022-06-05 22:48:15 -0500

    sincronizacion y validacion de tipo identificacion

--- src/controllers/tipoidentificacion.js
+++ src/controllers/tipoidentificacion.js
@@ -11,15 +11,14 @@ class Tipoidentificacion
         return results.rows;
     }
     async createTipoidentificacion(id_tipo_identificacion, descripcion) { 
-        let results = await db.query('SELECT * FROM tipo_identificacion WHERE descripcion = $1', [descripcion]).catch(console.log);
+        let results = await db.query('SELECT * FROM tipo_identificacion WHERE descripcion = $1 and id_tipo_identificacion = $2', [descripcion,id_tipo_identificacion]).catch(console.log);
         if (results.rowCount == 0) {     
-            await db
+            return await db
             .query('INSERT INTO tipo_identificacion (id_tipo_identificacion, descripcion) VALUES ($1, $2)', [
                 id_tipo_identificacion,
                 descripcion
             ])
-            .catch(console.log);
-          return;
+            .catch(console.log);       
         }
 }
 }
--- src/routers/tipoidentificacion.js
+++ src/routers/tipoidentificacion.js
@@ -3,22 +3,70 @@ const router = express.Router();
 const Tipoidentificacion = require('../controllers/tipoidentificacion');
 
 router.get('/tipoidentificacion_all', async (req,res) => { 
+    const response = newResponseJson();
+    response.msg = 'Listar todos los tipo identificacion';
+    let status = 200;
     let tipoidentificacion = await new Tipoidentificacion().getTipoidentificacion(); 
-    res.status(200).json(tipoidentificacion)
+    if (tipoidentificacion.length>0){
+        response.data = tipoidentificacion;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
+
 router.get('/tipoidentificacion/:descripcion', async (req,res) => {
-    let {descripcion} = req.params;    
+    const response = newResponseJson();
+    response.msg = 'Listar un tipo identificacion por descripcion';
+    let status = 200;
+    let {descripcion} = req.params;        
     let tipoidentificacion = await new Tipoidentificacion().getTipoidentificacionByDesc(descripcion);
-    res.status(200).json(tipoidentificacion)
+    if (tipoidentificacion.length>0){
+        response.data = tipoidentificacion;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
+
 router.post('/synchronization_tipoidentificacion', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Sincronización de tipo identificacion';
+    let status = 201;
     const {identificaciones } = req.body
+    let bandera = false;
     for (var i=0;i<identificaciones.length;i++){ 
         const {id_tipo_identificacion, descripcion } =  identificaciones[i]
-        await new Tipoidentificacion().createTipoidentificacion(id_tipo_identificacion, descripcion); 
-     };
-     
-     let tipoidentificacion_all= await new Tipoidentificacion().getTipoidentificacion();
-     res.status(200).json(tipoidentificacion_all)  
-});
+        result1 = await new Tipoidentificacion().createTipoidentificacion(id_tipo_identificacion, descripcion); 
+        console.log('primer insert', result1?.rowCount);
+        if (!result1?.rowCount || result1?.rowCount == 0) {
+            console.log('no se hizo el insert');
+            bandera = true;
+            break;        
+        }
+    }
+        if (identificaciones.length>0 && !bandera){
+            response.data = await new Tipoidentificacion().getTipoidentificacion();
+        }
+        else {
+            response.success = false;
+            status = 400;
+            response.msg = 'Error en la sincronización de tipo identificacion';
+    
+        }
+        res.status(status).json(response)
+    });
+    function newResponseJson() {
+        return {
+            success: true,
+            msg: "",
+            data: [],
+        };
+    }    
 module.exports = router;
