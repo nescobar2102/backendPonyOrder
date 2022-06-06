d9db5318525eacc6dea3ccce9086563c7ec712fb
haisbely <nunezhaisbely@gmail.com>
2022-06-05 22:35:50 -0500

    sincronizacion y validacion de tipoempresa

--- src/routers/tipoempresa.js
+++ src/routers/tipoempresa.js
@@ -3,22 +3,71 @@ const router = express.Router();
 const Tipoempresa = require('../controllers/tipoempresa');
 
 router.get('/tipoempresa_all', async (req,res) => { 
+    const response = newResponseJson();
+    response.msg = 'Listar todas las tipo de empresa';
+    let status = 200;
     let tipoempresa = await new Tipoempresa().getTipoempresa(); 
-    res.status(200).json(tipoempresa)
+    if (tipoempresa.length>0){
+        response.data = tipoempresa;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
-router.get('/tipoempresa/:descripcion/:nit', async (req,res) => {
-    let {descripcion,nit} = req.params;    
-    let tipoempresa = await new Tipoempresa().getTipoempresaByDesc(descripcion,nit);
-    res.status(200).json(tipoempresa)
+
+router.get('/tipoempresa/:nit/:descripcion', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Listar una tipo empresa por Nit y descripcion';
+    let status = 200;
+    let {nit,descripcion} = req.params;    
+    let tipoempresa = await new Tipoempresa().getTipoempresaByDesc(nit,descripcion);
+    if (tipoempresa.length>0){
+        response.data = tipoempresa;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
+
 router.post('/synchronization_tipoempresa', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Sincronización de tipo de empresa';
+    let status = 201;
     const {tipoempresas } = req.body
+    let bandera = false;
     for (var i=0;i<tipoempresas.length;i++){ 
         const { id_tipo_empresa, descripcion, nit} =  tipoempresas[i]
-        await new Tipoempresa().createTipoempresa(id_tipo_empresa, descripcion, nit); 
-     };
-     
-     let tipoempresa_all= await new Tipoempresa().getTipoempresa();
-     res.status(200).json(tipoempresa_all)  
-});
+        result1 = await new Tipoempresa().createTipoempresa(id_tipo_empresa, descripcion, nit); 
+        
+        console.log('primer insert', result1?.rowCount);
+        if (!result1?.rowCount || result1?.rowCount == 0) {
+            console.log('no se hizo el insert');
+            bandera = true;
+            break;        
+        }
+    }
+        if (tipoempresas.length>0 && !bandera){
+            response.data = await new Tipoempresa().getTipoempresa();
+        }
+        else {
+            response.success = false;
+            status = 400;
+            response.msg = 'Error en la sincronización de Tipo empresa';
+    
+        }
+        res.status(status).json(response)
+    });    
+    function newResponseJson() {
+    return {
+        success: true,
+        msg: "",
+        data: [],
+    };
+}
 module.exports = router;
