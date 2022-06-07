b1aab8a53d1a2044130ea349f15218be0b145b5c
haisbely <nunezhaisbely@gmail.com>
2022-06-06 21:38:12 -0500

    sincronizacion y validacion de medio de contacto

--- src/controllers/medioContacto.js
+++ src/controllers/medioContacto.js
@@ -6,23 +6,21 @@ class MedioContacto
         let results = await db.query(`SELECT * FROM medio_contacto  ORDER BY nit ASC`).catch(console.log); 
         return results.rows;
     }
-    async getMedioContactoByDesc(descripcion , nit) {
+    async getMedioContactoByDesc(descripcion,nit) {
         let results = await db.query('SELECT * FROM medio_contacto WHERE descripcion = $1 and nit = $2', [descripcion,nit]).catch(console.log); 
         return results.rows;
     }
     async createMedioContacto(id_medio_contacto, descripcion, nit) { 
-        let results = await db.query('SELECT * FROM medio_contacto WHERE nit = $1 and descripcion = $2', [descripcion,nit]).catch(console.log);
+        let results = await db.query('SELECT * FROM medio_contacto WHERE descripcion = $1 and nit = $2', [descripcion,nit]).catch(console.log);
         if (results.rowCount == 0) {     
-            await db
+            return await db
             .query('INSERT INTO medio_contacto (id_medio_contacto, descripcion, nit) VALUES ($1, $2, $3)', [
                 id_medio_contacto,
                 descripcion,
                 nit
             ])
-            .catch(console.log);
-          return;
+            .catch(console.log);         
         }
 }
-
 }
 module.exports = MedioContacto;
--- src/routers/medioContacto.js
+++ src/routers/medioContacto.js
@@ -3,23 +3,71 @@ const router = express.Router();
 const MedioContacto = require('../controllers/medioContacto');
 
 router.get('/medioContacto_all', async (req,res) => { 
+    const response = newResponseJson();
+    response.msg = 'Listar todos los medio Contacto';
+    let status = 200;
     let medioContacto = await new MedioContacto().getMedioContacto(); 
-    res.status(200).json(medioContacto)
+    if (medioContacto.length>0){
+        response.data = medioContacto;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
+
 router.get('/medioContacto/:descripcion/:nit', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Listar un medio Contacto Nit y descripcion';
+    let status = 200;
     let {descripcion,nit} = req.params;    
     let medioContacto = await new MedioContacto().getMedioContactoByDesc(descripcion,nit);
-    res.status(200).json(medioContacto)
+    if (medioContacto.length>0){
+        response.data = medioContacto;
+    }
+    else {
+        status = 404;
+        response.success = false;
+        response.mg = 'No existen registros';
+    }
+    res.status(status).json(response)
 });
+
 router.post('/synchronization_medioContacto', async (req,res) => {
+    const response = newResponseJson();
+    response.msg = 'Sincronización de Medio Contacto';
+    let status = 201;
     const {medio_contacto } = req.body
+    let bandera = false;
     for (var i=0;i<medio_contacto.length;i++){ 
         const {id_medio_contacto, descripcion, nit } =  medio_contacto[i]
-        await new MedioContacto().createMedioContacto(id_medio_contacto, descripcion, nit ); 
-     };
-     
-     let medioContacto_all= await new MedioContacto().getMedioContacto();
-     res.status(200).json(medioContacto_all)
-  
-});
+        result1  = await new MedioContacto().createMedioContacto(id_medio_contacto, descripcion, nit ); 
+          
+        console.log('primer insert', result1?.rowCount);
+        if (!result1?.rowCount || result1?.rowCount == 0) {
+            console.log('no se hizo el insert');
+            bandera = true;
+            break;        
+        }
+    }
+        if (medio_contacto.length>0 && !bandera){
+            response.data = await new MedioContacto().getMedioContacto();
+        }
+        else {
+            response.success = false;
+            status = 400;
+            response.msg = 'Error en la sincronización de Medio Contacto';
+    
+        }
+        res.status(status).json(response)
+    });
+    function newResponseJson() {
+        return {
+            success: true,
+            msg: "",
+            data: [],
+        };
+    }   
 module.exports = router;
