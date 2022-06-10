9dee014855eadb0ce7e4170570cb645018fe1bc6
haisbely <nunezhaisbely@gmail.com>
2022-06-09 21:59:24 -0500

    validaciones de tipo doc

--- src/controllers/tipodoc.js
+++ src/controllers/tipodoc.js
@@ -11,7 +11,7 @@ class Tipodoc
         return results.rows;
     }
     async createTipodoc(nit, id_empresa,id_sucursal, id_clase_doc, id_tipo_doc, consecutivo, descripcion ) { 
-        let results = await db.query('SELECT * FROM tipo_doc WHERE nit = $1 and descripcion = $2', [nit,descripcion]).catch(console.log);
+        let results = await db.query('SELECT * FROM tipo_doc WHERE nit = $1 and id_tipo_doc = $2', [nit,id_tipo_doc]).catch(console.log);
         if (results.rowCount == 0) {     
             return  await db
             .query('INSERT INTO tipo_doc (nit, id_empresa,id_sucursal, id_clase_doc, id_tipo_doc, consecutivo, descripcion) VALUES ($1, $2, $3, $4, $5, $6, $7 )', [
@@ -23,7 +23,9 @@ class Tipodoc
                 consecutivo,
                 descripcion,
             ])
-            .catch(console.log);      
+            .catch(console.log);    
+        }else{
+        return await db.query('UPDATE tipo_doc SET id_empresa = $1 ,id_sucursal = $2 ,id_clase_doc = $3, consecutivo = $4 ,descripcion = $5 WHERE nit = $6 and id_tipo_doc = $7', [id_empresa,id_sucursal,id_clase_doc,consecutivo,descripcion,nit,id_tipo_doc]).catch(console.log);   
         }
 }
 }
--- src/routers/tipodoc.js
+++ src/routers/tipodoc.js
@@ -9,8 +9,7 @@ router.get('/tipodoc_all', async (req,res) => {
     let tipodoc = await new Tipodoc().getTipodoc(); 
     if (tipodoc.length>0){
         response.data = tipodoc;
-    }
-    else {
+    }  else {
         status = 404;
         response.success = false;
         response.mg = 'No existen registros';
@@ -20,14 +19,13 @@ router.get('/tipodoc_all', async (req,res) => {
 
 router.get('/tipodoc/:nit/:descripcion', async (req,res) => {
     const response = newResponseJson();
-    response.msg = 'Listar un tipo de doc por Nit y  descripcion';
+    response.msg = 'Listar un tipo de doc por Nit y descripcion';
     let status = 200;
     let {nit,descripcion} = req.params;    
     let tipodoc = await new Tipodoc().getTipodocByDesc(nit,descripcion);
     if (tipodoc.length>0){
         response.data = tipodoc;
-    }
-    else {
+    } else {
         status = 404;
         response.success = false;
         response.mg = 'No existen registros';
@@ -46,28 +44,21 @@ router.post('/synchronization_tipodoc', async (req,res) => {
         result1 = await new Tipodoc().createTipodoc(nit, id_empresa,id_sucursal, id_clase_doc, id_tipo_doc, consecutivo, descripcion); 
         
         console.log('primer insert', result1?.rowCount);
-        if (!result1?.rowCount || result1?.rowCount == 0) {
-            console.log('no se hizo el insert');
+        if (!result1?.rowCount || result1?.rowCount == 0) {            
             bandera = true;
             break;        
         }
     }
         if (tipodocs.length>0 && !bandera){
             response.data = await new Tipodoc().getTipodoc();
-        }
-        else {
+        } else {
             response.success = false;
             status = 400;
-            response.msg = 'Error en la sincronización de Tipo doc';
-    
+            response.msg = 'Error en la Sincronización de Tipo doc';    
         }
         res.status(status).json(response)
     });
     function newResponseJson() {
-        return {
-            success: true,
-            msg: "",
-            data: [],
-        };
+        return {success: true,msg: "", data: []};
     }    
 module.exports = router;
