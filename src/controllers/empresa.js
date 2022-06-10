48c4c9927a18e513fd259111801d20ce7d8ad4ce
Norbelys Naguanagua <norbelysnaguanagua21@gmail.com>
2022-06-10 09:05:30 -0500

    validacion

--- src/controllers/empresa.js
+++ src/controllers/empresa.js
@@ -14,7 +14,7 @@ class Empresa {
 
     async createEmpresaUnica(nit, dv, razon_social, correo_electronico) {
         let results = await db.query('SELECT * FROM empresa WHERE nit = $1', [nit]).catch(console.log);
-        if (results.rowCount > 0) {
+        if (results?.rowCount > 0) {
             return results.rows;
         } else {
             return await db.query('INSERT INTO empresa (nit,dv,razon_social, correo_electronico) VALUES ($1, $2, $3, $4)', [nit, dv, razon_social, correo_electronico]).catch(console.log);
@@ -23,7 +23,7 @@ class Empresa {
 
     async createEmpresa(nit, razon_social, id_tipo_empresa, pre_actividad_economica, pre_cuenta, pre_medio_contacto, id_moneda, direccion, telefono, dv, fax, id_pais, id_depto, id_ciudad, regimen_tributario, flag_iva, flag_forma_pago_efectivo, correo_electronico, id_empresa) {
         let results = await db.query('SELECT * FROM empresa WHERE nit = $1', [nit]).catch(console.log);
-        if (results.rowCount > 0) {
+        if (results?.rowCount > 0) {
             return results.rows;
         } else {
             return await db.query('INSERT INTO empresa (nit,razon_social,id_tipo_empresa,pre_actividad_economica,pre_cuenta,pre_medio_contacto,id_moneda,direccion,telefono,dv,fax,id_pais,id_depto,id_ciudad,regimen_tributario,flag_iva,flag_forma_pago_efectivo,correo_electronico,id_empresa) VALUES ($1, $2, $3, $4,$5, $6, $7, $8, $9,$10, $11, $12, $13,$14, $15, $16, $17,$18, $19)', [
