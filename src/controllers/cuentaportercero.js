2047c9a85eae5ed6a64bf4ecb8378c62d2c5d8fd
haisbely <nunezhaisbely@gmail.com>
2022-06-09 23:40:37 -0500

    validacion de cuenta por tercero

--- src/controllers/cuentaportercero.js
+++ src/controllers/cuentaportercero.js
@@ -7,11 +7,14 @@ class Cuentaportercero
         return results.rows;
     }
     async getCuentaporterceroByNit(nit,tipo_doc) {
-        let results = await db.query('SELECT * FROM cuentas_por_tercero WHERE tipo_doc = $1 nit = $2', [nit,tipo_doc]).catch(console.log); 
+        let results = await db.query('SELECT * FROM cuentas_por_tercero WHERE nit = $1 and tipo_doc = $2', [nit,tipo_doc]).catch(console.log); 
         return results.rows;
-    }
+    }    
+    async deleteCuentaportercero() {
+        await db.query(`DELETE FROM cuentas_por_tercero`).catch(console.log);             
+  }
     async createCuentaportercero(nit,id_empresa,id_sucursal,tipo_doc,numero,cuota,dias,id_tercero,id_vendedor,id_sucursal_tercero,fecha,vencimiento,credito,dctomax,cuota_cruce,debito,id_destino,id_proyecto) { 
-        let results = await db.query('SELECT * FROM cuentas_por_tercero WHERE nit = $1', [nit]).catch(console.log);
+        let results = await db.query('SELECT * FROM cuentas_por_tercero WHERE nit = $1 and numero = $2', [nit,numero]).catch(console.log);
         if (results.rowCount == 0) {     
             return await db
             .query('INSERT INTO cuentas_por_tercero (nit,id_empresa,id_sucursal,tipo_doc,numero,cuota,dias,id_tercero,id_vendedor,id_sucursal_tercero,fecha,vencimiento,credito,dctomax,cuota_cruce,debito,id_destino,id_proyecto) VALUES ($1, $2, $3,$4, $5, $6,$7, $8, $9,$10, $11, $12,$13, $14, $15,$16, $17,$18)', [
@@ -19,7 +22,8 @@ class Cuentaportercero
                 id_empresa,
                 id_sucursal,
                 tipo_doc,
-                numero,cuota,
+                numero,
+                cuota,
                 dias,
                 id_tercero,
                 id_vendedor,
@@ -33,7 +37,7 @@ class Cuentaportercero
                 id_destino,
                 id_proyecto
             ])
-            .catch(console.log); 
+            .catch(console.log);       
         }
 }
 }
