fc78bccb804857cb96ae68ffc3f498e2e9c79e4a
haisbely <nunezhaisbely@gmail.com>
2022-06-22 18:49:11 -0500

    cuotas de ventas

--- src/controllers/cuotaventas.js
+++ src/controllers/cuotaventas.js
@@ -1,20 +1,22 @@
 const db = require('../config/db')
 
-class Cuotaventas 
-    {
+class Cuotaventas  {
         async getCuotaventas() {
             let results = await db.query(`SELECT * FROM cuota_venta ORDER BY nit ASC`).catch(console.log); 
             return results.rows;
         }
-        async getCuotaventasByNit(nit,nombre) {
-            let results = await db.query('SELECT * FROM cuota_venta WHERE  nit = $1 and nombre = $2', [nit,nombre]).catch(console.log); 
+        async getCuotaventasByNit(nit) {
+            let results = await db.query('SELECT * FROM cuota_venta WHERE  nit = $1', [nit]).catch(console.log); 
+            return results.rows;
+        }
+        async getCuotaventasNitId(nit,id_linea) {
+            let results = await db.query('SELECT * FROM cuota_venta WHERE  nit = $1 and id_linea =$2', [nit,id_linea]).catch(console.log); 
             return results.rows;
         }
         async createCuotaventas(nit,id_vendedor,id_suc_vendedor,venta,cuota,id_linea,nombre) { 
-            let results = await db.query('SELECT * FROM Cuota_venta WHERE nit = $1 and nombre =$2 and id_linea =$3', [nit,nombre,id_linea]).catch(console.log);
-            if (results.rowCount == 0) {     
-                return await db
-                .query('INSERT INTO cuota_venta (nit,id_vendedor,id_suc_vendedor,venta,cuota,id_linea,nombre) VALUES ($1, $2, $3, $4, $5,$6, $7)', [
+            let response
+            try { 
+                const insert = await db.query('INSERT INTO cuota_venta (nit,id_vendedor,id_suc_vendedor,venta,cuota,id_linea,nombre) VALUES ($1, $2, $3, $4, $5,$6, $7)', [
                     nit,
                     id_vendedor,
                     id_suc_vendedor,
@@ -22,11 +24,12 @@ class Cuotaventas
                     cuota,
                     id_linea,
                     nombre
-                ])
-                .catch(console.log);  
-            }else{
-                return await db.query('UPDATE cuota_venta SET venta = $1 ,cuota = $2 ,nombre = $3 ,id_vendedor = $4 , id_suc_vendedor = $5 WHERE nit = $6 and id_linea= $7', [venta,cuota,nombre,id_vendedor,id_suc_vendedor,nit,id_linea]).catch(console.log);   
-            }
-    }    
+                ]);
+                response = insert;
+        }  catch (err) {
+            response = err;
+        }
+            return response
+        }
     }
 module.exports = Cuotaventas;
